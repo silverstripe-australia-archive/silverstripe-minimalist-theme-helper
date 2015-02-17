d3e8dc8923a650f7a4421a947e8d575b7ebd2d85
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-02-17 22:31:19 +1100

    Added basic module to support the ssau-minimalist theme

---
+++ code/MinimalistThemeExtensions.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * 
+ *
+ * @author <marcus@silverstripe.com.au>
+ * @license BSD License http://www.silverstripe.org/bsd-license
+ */
+class PageControllerThemeExtension extends Extension {
+	public function onAfterInit() {
+		Requirements::block(THIRDPARTY_DIR .'/jquery/jquery.js');
+		Requirements::javascript('frontend-dashboards/javascript/jquery-1.10.2.min.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-cookie/jquery.cookie.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery-ui.js');
+		//Requirements::javascript('frontend-dashboards/javascript/jquery-migrate-1.2.1.min.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
+		Requirements::block('frontend-dashboards/javascript/dashboards.js');
+		Requirements::javascript('themes/ssau-minimalist/js/modernizr.js');
+		Requirements::javascript('themes/ssau-minimalist/js/foundation.min.js');
+	}
+}
