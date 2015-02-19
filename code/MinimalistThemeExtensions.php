ec21d792bae09cb1949515496526b1898e22363d
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-02-20 10:59:09 +1100

    FIX Include jquery-migrate

--- code/MinimalistThemeExtensions.php
+++ code/MinimalistThemeExtensions.php
@@ -7,13 +7,13 @@
  * @license BSD License http://www.silverstripe.org/bsd-license
  */
 class PageControllerThemeExtension extends Extension {
-	public function onAfterInit() {
+	public function onBeforeInit() {
 		if (Config::inst()->get('SSViewer', 'theme') == 'ssau-minimalist') {
 			Requirements::block(THIRDPARTY_DIR .'/jquery/jquery.js');
 			Requirements::javascript('frontend-dashboards/javascript/jquery-1.10.2.min.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-cookie/jquery.cookie.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery-ui.js');
-			//Requirements::javascript('frontend-dashboards/javascript/jquery-migrate-1.2.1.min.js');
+			Requirements::javascript('frontend-dashboards/javascript/jquery-migrate-1.2.1.min.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
 			Requirements::block('frontend-dashboards/javascript/dashboards.js');
 			Requirements::javascript('themes/ssau-minimalist/js/modernizr.js');
