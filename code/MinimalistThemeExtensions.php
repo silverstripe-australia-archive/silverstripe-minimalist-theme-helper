9889ae074f1d820571e7832e5bbb97561422fe78
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-03-04 11:40:23 +1100

    CONFIG Add config option for requiring JS
    Created a config setting for including the requirements statements, to allow
    a project to specify these instead
    
    Added PreLayout and PostLayout hook points for projects to provide their own
    specific hooks into the pre/post layout process

--- code/MinimalistThemeExtensions.php
+++ code/MinimalistThemeExtensions.php
@@ -7,8 +7,10 @@
  * @license BSD License http://www.silverstripe.org/bsd-license
  */
 class PageControllerThemeExtension extends Extension {
+	private static $include_requirements = true;
+	
 	public function onBeforeInit() {
-		if (Config::inst()->get('SSViewer', 'theme') == 'ssau-minimalist') {
+		if (Config::inst()->get('PageControllerThemeExtension', 'include_requirements') && Config::inst()->get('SSViewer', 'theme') == 'ssau-minimalist') {
 			Requirements::block(THIRDPARTY_DIR .'/jquery/jquery.js');
 			Requirements::javascript('frontend-dashboards/javascript/jquery-1.10.2.min.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-cookie/jquery.cookie.js');
@@ -18,6 +20,21 @@ class PageControllerThemeExtension extends Extension {
 			Requirements::block('frontend-dashboards/javascript/dashboards.js');
 			Requirements::javascript('themes/ssau-minimalist/js/modernizr.js');
 			Requirements::javascript('themes/ssau-minimalist/js/foundation.min.js');
+			
+			Requirements::javascript('themes/ssau-minimalist/js/jquery.slides.min.js');
+			Requirements::javascript('themes/ssau-minimalist/js/general.js');
+			Requirements::javascript('intranet-sis/javascript/info-lists.js');
 		}
+	}
+	
+	/**
+	 * Allows concrete controllers to provide implementations
+	 */
+	public function PreLayout() {
+		
+	}
+	
+	public function PostLayout() {
+		
 	}
 }
