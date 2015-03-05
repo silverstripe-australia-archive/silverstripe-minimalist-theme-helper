b795792f35848ef04a6a9617b41287ca12f07a40
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-03-05 11:03:02 +1100

    Include the minimalist theme version of dashboards.js if using that theme

--- code/MinimalistThemeExtensions.php
+++ code/MinimalistThemeExtensions.php
@@ -11,19 +11,28 @@ class PageControllerThemeExtension extends Extension {
 	
 	public function onBeforeInit() {
 		if (Config::inst()->get('PageControllerThemeExtension', 'include_requirements') && Config::inst()->get('SSViewer', 'theme') == 'ssau-minimalist') {
+			// we'll use the frontend dashboard jquery
 			Requirements::block(THIRDPARTY_DIR .'/jquery/jquery.js');
+			
+			// block frontend dashboards' js, we have our own
+			Requirements::block('frontend-dashboards/javascript/dashboards.js');
+			
 			Requirements::javascript('frontend-dashboards/javascript/jquery-1.10.2.min.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-cookie/jquery.cookie.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery-ui.js');
 			Requirements::javascript('frontend-dashboards/javascript/jquery-migrate-1.2.1.min.js');
 			Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
-			Requirements::block('frontend-dashboards/javascript/dashboards.js');
+			
 			Requirements::javascript('themes/ssau-minimalist/js/modernizr.js');
 			Requirements::javascript('themes/ssau-minimalist/js/foundation.min.js');
 			
 			Requirements::javascript('themes/ssau-minimalist/js/jquery.slides.min.js');
 			Requirements::javascript('themes/ssau-minimalist/js/general.js');
 			Requirements::javascript('intranet-sis/javascript/info-lists.js');
+
+			if ($this->owner instanceof Dashlet_Controller) {
+				Requirements::javascript('themes/ssau-minimalist/js/dashboards.js');
+			}
 		}
 	}
 	
